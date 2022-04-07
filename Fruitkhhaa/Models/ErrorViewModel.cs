f32267b3536d50b49fb04075bdb1d1c54fbcd6a4
Пользователь <aytacrustmli93@gmail.com>
2022-04-07 21:56:40 +0400

    07.04.2022

---
+++ Fruitkhhaa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fruitkhhaa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
